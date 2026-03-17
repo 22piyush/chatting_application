d7102a034d62bdd171179d77d22cf4dcd5e8f245
22piyush <piyushaglawe05@gmail.com>
2026-03-17 22:14:59 +0530

    done

---
+++ server/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+
+const app = express();
+
+// Load environment variables
+dotenv.config();
+
+// Middlewares
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // your frontend URL
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// File upload middleware
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/"
+}));
+
+// Test route
+app.get('/', (req, res) => {
+    res.send("API is running...");
+});
+
+export default app;
