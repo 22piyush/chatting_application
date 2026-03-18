c0072a1f4fe97e7ab81918a87a690214e00c8fd5
22piyush <piyushaglawe05@gmail.com>
2026-03-18 22:44:51 +0530

    done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -62,6 +62,40 @@ export const signup = catchAsyncError(async (req, res, next) => {
 
 export const signin = catchAsyncError(async (req, res, next) => {
 
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide email and password"
+        });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid email or password"
+        });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        return res.status(400).json({
+            success: false,
+            message: "Invalid email or password"
+        });
+    }
+
+    generateJWTToken(
+        user,
+        "Login successful",
+        200,
+        res
+    );
+
 });
 
 
