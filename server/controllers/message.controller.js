4cf6a2460b85fde3661157874bb5e61a35720bfb
22piyush <piyushaglawe05@gmail.com>
2026-03-19 21:37:54 +0530

    done

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,11 +1,27 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js";
 import { User } from "../models/user.model.js";
+import { Message } from "../models/message.model.js";
+
 
 export const getAllUsers = catchAsyncError(async (req, res, next) => {
 
+    const userId = req.user._id;
+
+    const users = await User.find({
+        _id: { $ne: userId } //exclude current user
+    }).select("-password");
+
+    return res.status(200).json({
+        success: true,
+        users
+    });
+
 });
 
+
 export const getMessages = catchAsyncError(async (req, res, next) => {
+    
+    
 
 });
 
