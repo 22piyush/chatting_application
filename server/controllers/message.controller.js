1f313d36ceee006a16346d3299280db687bd9cb9
22piyush <piyushaglawe05@gmail.com>
2026-03-19 22:02:15 +0530

    done

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -18,13 +18,39 @@ export const getAllUsers = catchAsyncError(async (req, res, next) => {
 
 });
 
-
 export const getMessages = catchAsyncError(async (req, res, next) => {
-    
-    
 
+    const receiverId = req.params.id;
+    const myId = req.user._id;
+
+    const receiver = await User.findById(receiverId);
+    if (!receiver) {
+        return res.status(400).json({
+            success: false,
+            message: "Receiver ID Invalid."
+        });
+    }
+
+    const messages = await Message.find({
+        $or: [
+            { senderId: myId, receiverId: receiverId },
+            { senderId: receiverId, receiverId: myId }
+        ]
+    })
+        // .populate("senderId", "fullName avatar")
+        // .populate("receiverId", "fullName avatar")
+        .sort({ createdAt: 1 });
+
+    return res.status(200).json({
+        success: true,
+        messages
+    });
 });
 
 export const sendMessages = catchAsyncError(async (req, res, next) => {
+
+    const {text} = req.body;
+
+    
 
 });
