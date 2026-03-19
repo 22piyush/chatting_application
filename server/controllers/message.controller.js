d78c91f18e7ed6918ce91b86f504cf56fdc6cbd1
22piyush <piyushaglawe05@gmail.com>
2026-03-19 21:30:22 +0530

    done

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,14 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.middleware.js";
+import { User } from "../models/user.model.js";
+
+export const getAllUsers = catchAsyncError(async (req, res, next) => {
+
+});
+
+export const getMessages = catchAsyncError(async (req, res, next) => {
+
+});
+
+export const sendMessages = catchAsyncError(async (req, res, next) => {
+
+});
