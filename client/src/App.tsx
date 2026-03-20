2f6fd2d1282c1c669f8b492562e16b0e1fb43a6e
22piyush <piyushaglawe05@gmail.com>
2026-03-20 21:13:09 +0530

    done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,23 @@ import { useAppDispatch, useAppSelector } from "./hooks/hooks";
 import { getUser, setOnlineUsers } from "./store/slices/authSlice";
 import { connectSocket, disconnectSocket } from "./lib/socket";
 
+// router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// components
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+
+// icons
+import { Loader } from "lucide-react";
+
+//toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   const dispatch = useAppDispatch();
 
@@ -10,7 +27,7 @@ function App() {
     (state) => state.auth
   );
 
-  //get user
+  // get user
   useEffect(() => {
     dispatch(getUser());
   }, [dispatch]);
@@ -25,18 +42,50 @@ function App() {
       dispatch(setOnlineUsers(users));
     });
 
-    //cleanup
     return () => {
-      disconnectSocket(); //correct
+      disconnectSocket();
     };
   }, [authUser, dispatch]);
 
-  if (isCheckingAuth) return <div>Loading...</div>;
+  // loader
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="animate-spin w-10 h-10" />
+      </div>
+    );
+  }
 
   return (
-    <div>
-      {authUser ? `Welcome ${authUser.name}` : "Not logged in"}
-    </div>
+    <BrowserRouter>
+      {authUser && <Navbar />}
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="/login"
+          element={!authUser ? <Login /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/register"
+          element={!authUser ? <Register /> : <Navigate to="/" />}
+        />
+
+        {/*FIXED */}
+        <Route
+          path="/profile"
+          element={authUser ? <Profile /> : <Navigate to="/login" />}
+        />
+      </Routes>
+
+      {/*Toast container */}
+      <ToastContainer position="top-right" autoClose={3000} />
+    </BrowserRouter>
   );
 }
 
