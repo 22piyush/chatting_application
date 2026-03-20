afa810fa2764fb684d0752aad61187f65daa0b6f
22piyush <piyushaglawe05@gmail.com>
2026-03-20 22:16:10 +0530

    done

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../../store/slices/authSlice";
 
 function Login() {
 
-  
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email:"",
+    password: ""
+  });
+
+    const { isLoggingIn } = useAppSelector((state) => state.auth);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const handleSubmit =(e)=>{
+      e.preventDefault();
+      dispatch(login(formData));
+    }
 
   return (
     <div>Login</div>
