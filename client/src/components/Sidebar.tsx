d15efc543a270ccdd3a3e51ce1d339923df324cc
22piyush <piyushaglawe05@gmail.com>
2026-03-23 21:31:04 +0530

    done

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+
 
 function Sidebar() {
+
+  const [showOnlineOnly, setShowOnlineOnly] = use
+
   return (
     <div>Sidebar</div>
   )
