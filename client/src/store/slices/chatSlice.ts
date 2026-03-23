ab1963a51f1f84401a649cc4ccdc1bae0c823556
22piyush <piyushaglawe05@gmail.com>
2026-03-23 21:09:30 +0530

    done

--- client/src/store/slices/chatSlice.ts
+++ client/src/store/slices/chatSlice.ts
@@ -4,3 +4,22 @@ import { axiosInstance } from "../../lib/axios";
 import { connectSocket, disconnectSocket } from "../../lib/socket";
 
 
+// Auth state type
+interface AuthState {
+  authUser: User | null;
+  isSigningIn: boolean;
+  isLoggingIn: boolean;
+  isUpdatingProfile: boolean;
+  isCheckingAuth: boolean;
+  onlineUsers: string[];
+}
+
+// Initial state
+const initialState: AuthState = {
+  authUser: null,
+  isSigningIn: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+  onlineUsers: [],
+};
