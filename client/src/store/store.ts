ea91a0d1fcabf5067b93c011b0a03f540d60474c
22piyush <piyushaglawe05@gmail.com>
2026-03-23 21:21:02 +0530

    done

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-
+import chatReducer from "./slices/authSlice";
 // store
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    chat: chatReducer
   },
 });
 
